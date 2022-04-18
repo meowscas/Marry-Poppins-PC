0315aa62bd192f383c9847476d920078b4234638
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2022-04-18 01:50:53 -0700

    Upload source

---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace MarryPoppins
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        bool gotGravity = false;
+        public static bool umbrellaOpened;
+        Vector3 originalGravity;
+
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            if (!gotGravity)
+            {
+                originalGravity = Physics.gravity;
+                gotGravity = true;
+            }
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void FixedUpdate()
+        {
+            if(inRoom)
+            {
+                if (umbrellaOpened) Physics.gravity = new Vector3(0f, -3.0f, 0f);
+                else Physics.gravity =  originalGravity;
+            }
+        }
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MarryPoppins
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.buzzbzzzbzzbzzzthe18th.gorillatag.marrypoppins";
+        public const string Name = "MarryPoppins";
+        public const string Version = "1.0.0";
+    }
+}
